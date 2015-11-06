11caa62155cdfb6b41c088772a87e0a4ff1c0be1
lucien-le-foll <lucien.lefoll@gmail.com>
2015-11-06 12:48:03 +0000

    Original Commit
    Creation of the repository, in order to make branchs and try out some
    styling.

---
+++ ca01_form.php
@@ -0,0 +1,75 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Car Rental Application</title>
+		<meta charset="utf-8">
+		<?php
+			$categories = scandir('cars');
+			unset($categories[0]);
+			unset($categories[1]);
+			if (isset($_POST['category'])){
+				if($_POST['category'] == '*'){
+
+				}
+				else {
+					$requiredCategory = $_POST['category'];
+				}				
+			}
+		?>
+	</head>
+	<body>
+		<h1>Mootown Rentals</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi eos accusamus nemo doloremque saepe molestiae, voluptas maxime, molestias recusandae eligendi et dolor ullam hic minima repellat optio alias, ratione quisquam!</p>
+		<h2>Filter</h2>
+		<form action="ca01_form.php" method="post">
+			<select name="category" id="category">
+				<option value="*">All categories</option>
+				<?php foreach($categories as $key => $category): ?>
+					<option value='<?= $key ?>'><?= $category ?></option>
+				<?php endforeach; ?>		
+			</select>
+			<button type="submit">Filter</button>
+		</form>
+		<form action="ca01_result.php" method="post">
+			<?php foreach($categories as $key => $category): ?>
+				<?php if(isset($requiredCategory)): ?>
+					<?php if($key == $requiredCategory): ?>
+						<h2><?= $category ?></h2>
+						<?php foreach(glob("cars\\".$category."\\*.jpg") as $link): ?>
+							<div>
+								<img src="<?= $link ?>" alt="dummy">
+								<?php
+									$dir = "cars\\".$category."\\";
+									$var = str_replace($dir, '', $link);
+									$rough_name = str_replace('.jpg', '', $var);
+									$name = str_replace('-', ' ', $rough_name);
+									echo "<h3>".ucwords($name)."</h3>";
+								?>
+								<label for="<?= $rough_name ?>">Pick this car</label>
+								<input type="radio" name="chosenCar" id="<?= $rough_name ?>" value='{"name" : "<?= $rough_name ?>", "category" : "<?= $category ?>"}'>
+							</div>							
+						<?php endforeach; ?>
+						<button type="submit">Send</button>
+					<?php endif; ?>
+				<?php else: ?>
+					<h2><?= $category ?></h2>
+					<?php foreach(glob("cars\\".$category."\\*.jpg") as $link): ?>
+						<div>
+							<img src="<?= $link ?>" alt="dummy">
+							<?php
+								$dir = "cars\\".$category."\\";
+								$var = str_replace($dir, '', $link);
+								$rough_name = str_replace('.jpg', '', $var);
+								$name = str_replace('-', ' ', $rough_name);
+								echo "<h3>".ucwords($name)."</h3>";
+							?>
+							<label for="<?= $rough_name ?>">Pick this car</label>
+							<input type="radio" name="chosenCar" id="<?= $rough_name ?>" value='{"name" : "<?= $rough_name ?>", "category" : "<?= $category ?>"}'>
+						</div>						
+					<?php endforeach; ?>
+					<button type="submit">Send</button>
+				<?php endif; ?>					
+			<?php endforeach; ?>	
+		</form>
+	</body>
+</html>
---
+++ ca01_result.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Car Rental Application</title>
+</head>
+<body>
+	<h1>Mootown Rentals</h1>
+	<p><a href="ca01_form.php">Step 1</a> > <u>Step 2</u></p> 
+	<?php
+		$chosenCar = json_decode($_POST['chosenCar'], true);
+		echo $chosenCar['category'];
+		$name = str_replace('-', ' ', $chosenCar['name']);
+		$link = "cars\\".$chosenCar['category']."\\".$chosenCar['name'].".jpg";
+	?>
+	<p>You picked the car :</p>
+	<h3><?= ucwords($name) ?></h3>
+	<img src="<?= $link ?>" alt="<?= $chosenCar['name'] ?>">
+</body>
+</html>
